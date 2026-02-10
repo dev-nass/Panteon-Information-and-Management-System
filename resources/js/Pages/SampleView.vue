a495e0d82cd3e75c86ce9d966d299621832f9fe4
dev-nass <jonasvincemaca@gmail.com>
2026-02-11 00:08:15 +0800

    add: about us section

--- resources/js/Pages/SampleView.vue
+++ resources/js/Pages/SampleView.vue
@@ -4,7 +4,7 @@ import Navbar from "@/Components/Navbar.vue";
 <template>
     <Navbar />
     <!-- Hero -->
-    <div class="px-4 sm:px-6 lg:px-4">
+    <div class="container mx-auto px-4 sm:px-6 lg:px-4">
         <div
             class="relative h-150 md:h-[90dvh] flex flex-col bg-[url('/images/hero-background.png')] bg-cover bg-center bg-no-repeat rounded-2xl overflow-hidden"
         >
@@ -47,4 +47,82 @@ import Navbar from "@/Components/Navbar.vue";
         </div>
     </div>
     <!-- End Hero -->
+
+    <div
+        class="container mx-auto flex flex-col items-center px-4 py-5 lg:px-4 lg:py-15"
+    >
+        <article
+            class="py-10 text-xl leading-relaxed lg:text-2xl text-center md:w-[50ch] lg:w-[60ch]"
+        >
+            <span class="font-bold">Panteon De Dasmariñas Cemetery</span> is a
+            long-established memorial ground located in the heart of Dasmariñas,
+            Cavite. Serving generations of families, it features developed
+            burial areas with planned plots and additional spaces reserved for
+            future expansion. The cemetery remains an important place of
+            remembrance, honoring the heritage and stories of the community.
+        </article>
+        <!-- Features -->
+        <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+            <!-- Grid -->
+            <div
+                class="grid grid-cols-2 justify-items-center gap-6 items-around sm:gap-12 lg:grid-cols-3 lg:gap-8"
+            >
+                <!-- Stats -->
+                <div>
+                    <h4
+                        class="text-lg font-semibold text-gray-800 sm:text-xl dark:text-neutral-200"
+                    >
+                        Deceased Records
+                    </h4>
+                    <p
+                        class="mt-2 text-4xl font-bold text-green-600 sm:mt-3 sm:text-6xl dark:text-green-500"
+                    >
+                        20K+
+                    </p>
+                    <p class="mt-1 text-gray-500 dark:text-neutral-400">
+                        in fulfilling orders
+                    </p>
+                </div>
+                <!-- End Stats -->
+
+                <!-- Stats -->
+                <div>
+                    <h4
+                        class="text-lg font-semibold text-gray-800 sm:text-xl dark:text-neutral-200"
+                    >
+                        Open Hours
+                    </h4>
+                    <p
+                        class="mt-2 text-4xl font-bold text-green-600 sm:mt-3 sm:text-6xl dark:text-green-500"
+                    >
+                        6-18
+                    </p>
+                    <p class="mt-1 text-gray-500 dark:text-neutral-400">
+                        GMT +8
+                    </p>
+                </div>
+                <!-- End Stats -->
+
+                <!-- Stats -->
+                <div>
+                    <h4
+                        class="text-lg font-semibold text-gray-800 sm:text-xl dark:text-neutral-200"
+                    >
+                        Happy customer
+                    </h4>
+                    <p
+                        class="mt-2 text-4xl font-bold text-green-600 sm:mt-3 sm:text-6xl dark:text-green-500"
+                    >
+                        85%
+                    </p>
+                    <p class="mt-1 text-gray-500 dark:text-neutral-400">
+                        this year alone
+                    </p>
+                </div>
+                <!-- End Stats -->
+            </div>
+            <!-- End Grid -->
+        </div>
+        <!-- End Features -->
+    </div>
 </template>
