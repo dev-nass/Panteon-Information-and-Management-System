2735429e6d2b8aa966e51cd31840ac0c25ca4c9d
dev-nass <jonasvincemaca@gmail.com>
2026-02-10 22:55:06 +0800

    add: hero section v2

---
+++ resources/js/Pages/SampleView.vue
@@ -0,0 +1,48 @@
+<script setup>
+import Navbar from "@/Components/Navbar.vue";
+</script>
+<template>
+    <Navbar />
+    <!-- Hero -->
+    <div class="px-4 sm:px-6 lg:px-4">
+        <div
+            class="relative h-150 md:h-[90dvh] flex flex-col bg-[url('/images/hero-background.png')] bg-cover bg-center bg-no-repeat rounded-2xl overflow-hidden"
+        >
+            <!-- Overlay -->
+            <div class="absolute inset-0 bg-black/40 rounded-2xl"></div>
+
+            <!-- Content - Add relative z-10 here -->
+            <div
+                class="relative z-10 h-full w-full flex flex-col md:flex-col-reverse justify-between pt-10 lg:pt-20 mt-auto md:w-2/3 md:max-w-xl ps-5 pe-5 pb-5 md:ps-10 md:pb-10"
+            >
+                <h1
+                    class="bona-nova-heading text-center md:text-left text-4xl md:text-6xl lg:text-8xl text-white"
+                >
+                    Panteon De Dasmariñas
+                </h1>
+                <div class="text-center md:text-left">
+                    <p class="text-white text-base md:text-lg leading-relaxed">
+                        A historic ground dedicated to honoring lives,
+                        preserving cultural heritage, and providing
+                        compassionate support to families across generations.
+                    </p>
+                    <div
+                        class="flex mt-5 gap-4 flex-wrap justify-center md:justify-start"
+                    >
+                        <button
+                            class="bg-blue-600 hover:bg-blue-700 text-white px-8 py-3 rounded-lg font-semibold transition"
+                        >
+                            View Map
+                        </button>
+                        <button
+                            class="border-2 border-white hover:bg-white hover:text-gray-900 text-white px-8 py-3 rounded-lg font-semibold transition"
+                        >
+                            Contact Us
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Hero -->
+</template>
--- resources/js/Pages/WelcomeView.vue
+++ resources/js/Pages/WelcomeView.vue
@@ -22,12 +22,14 @@ import Navbar from "@/Components/Navbar.vue";
                 class="relative z-10 flex items-center justify-center h-full border border-red-500"
             >
                 <div
-                    class="border border-red-500 text-center text-white px-6 mt-20 flex flex-col justify-between gap-y-50 md:flex-col-reverse"
+                    class="border text-center text-white px-6 mt-20 flex flex-col justify-between gap-y-50 md:flex-col-reverse"
                 >
-                    <h1 class="text-5xl md:text-7xl font-bold mb-6">
+                    <h1
+                        class="text-5xl md:text-8xl mb-6 font-extralight lg:text-[7rem] bona-nova-heading"
+                    >
                         Panteon De Dasmariñas
                     </h1>
-                    <article class="md:flex md:flex-col-reverse">
+                    <article class="md:flex md:flex-col-reverse md:w-md">
                         <div class="flex gap-4 justify-center md:justify-start">
                             <button
                                 class="bg-blue-600 hover:bg-blue-700 text-white px-8 py-3 rounded-lg font-semibold transition-colors"
@@ -41,11 +43,12 @@ import Navbar from "@/Components/Navbar.vue";
                             </button>
                         </div>
                         <p
-                            class="text-xl md:text-2xl mb-8 max-w-3xl mx-auto md:text-left md:mx-0"
+                            class="text-xl md:text-2xl mb-8 max-w-3xl mx-auto md:text-left md:mx-0 leading-relaxed lg:w-[40ch] lg:text-xl"
                         >
                             A historic ground dedicated to honoring lives,
-                            preserving heritage, and serving families with
-                            compassion.
+                            preserving cultural heritage, and offering
+                            compassionate care and guidance to families during
+                            life’s most meaningful moments.
                         </p>
                     </article>
                 </div>
