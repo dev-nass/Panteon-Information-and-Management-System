68331b812947481a24d2ef0da79ed1ee441def1e
dev-nass <jonasvincemaca@gmail.com>
2026-02-11 15:33:59 +0800

    add: gallery section

--- resources/js/Pages/SampleView.vue
+++ resources/js/Pages/SampleView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Navbar from "@/Components/Navbar.vue";
 import AboutUsSection from "@/Components/Welcome/AboutUsSection.vue";
+import GallerySection from "@/Components/Welcome/GallerySection.vue";
 import HeroSection from "@/Components/Welcome/HeroSection.vue";
 </script>
 <template>
     <Navbar />
     <HeroSection />
     <AboutUsSection />
+    <GallerySection />
 </template>
