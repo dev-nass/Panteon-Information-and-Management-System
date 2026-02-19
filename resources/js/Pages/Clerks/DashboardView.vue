cd4c4ff47985f2664ddc614a3f62bad5a4df5c67
dev-nass <jonasvincemaca@gmail.com>
2026-02-20 01:03:01 +0800

    add: newbar link

--- resources/js/Pages/Clerks/DashboardView.vue
+++ resources/js/Pages/Clerks/DashboardView.vue
@@ -144,21 +144,45 @@ import SidebarLink from "@/Components/Dashboard/SidebarLink.vue";
                         <li>
                             <SidebarLink isActive="true">
                                 <svg
-                                    class="size-4"
                                     xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
+                                    width="20"
+                                    height="20"
                                     viewBox="0 0 24 24"
                                     fill="none"
                                     stroke="currentColor"
                                     stroke-width="2"
                                     stroke-linecap="round"
                                     stroke-linejoin="round"
+                                    class="lucide lucide-layout-dashboard-icon lucide-layout-dashboard"
                                 >
-                                    <path
-                                        d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"
+                                    <rect
+                                        width="7"
+                                        height="9"
+                                        x="3"
+                                        y="3"
+                                        rx="1"
                                     />
-                                    <polyline points="9 22 9 12 15 12 15 22" />
+                                    <rect
+                                        width="7"
+                                        height="5"
+                                        x="14"
+                                        y="3"
+                                        rx="1"
+                                    />
+                                    <rect
+                                        width="7"
+                                        height="9"
+                                        x="14"
+                                        y="12"
+                                        rx="1"
+                                    />
+                                    <rect
+                                        width="7"
+                                        height="5"
+                                        x="3"
+                                        y="16"
+                                        rx="1"
+                                    />
                                 </svg>
                                 <span class="hs-overlay-minified:hidden"
                                     >Dashboard</span
@@ -231,6 +255,37 @@ import SidebarLink from "@/Components/Dashboard/SidebarLink.vue";
                                 </svg>
                                 <span class="hs-overlay-minified:hidden"
                                     >Interactive Map</span
+                                >
+                            </SidebarLink>
+                        </li>
+                        <li>
+                            <SidebarLink>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="lucide lucide-library-big-icon lucide-library-big"
+                                >
+                                    <rect
+                                        width="8"
+                                        height="18"
+                                        x="3"
+                                        y="3"
+                                        rx="1"
+                                    />
+                                    <path d="M7 3v18" />
+                                    <path
+                                        d="M20.4 18.9c.2.5-.1 1.1-.6 1.3l-1.9.7c-.5.2-1.1-.1-1.3-.6L11.1 5.1c-.2-.5.1-1.1.6-1.3l1.9-.7c.5-.2 1.1.1 1.3.6Z"
+                                    />
+                                </svg>
+                                <span class="hs-overlay-minified:hidden"
+                                    >Manage Deceased</span
                                 >
                             </SidebarLink>
                         </li>
