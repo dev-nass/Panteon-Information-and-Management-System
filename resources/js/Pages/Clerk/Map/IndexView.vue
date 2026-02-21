0a94e7c51ad5aa672ed73be867f0547f0ef6c692
dev-nass <jonasvincemaca@gmail.com>
2026-02-22 02:31:50 +0800

    ref: map layout

--- resources/js/Pages/Clerk/Map/IndexView.vue
+++ resources/js/Pages/Clerk/Map/IndexView.vue
@@ -25,9 +25,9 @@ onBeforeUnmount(() => {
     <section id="map-wrapper" class="relative" style="height: 98vh">
         <div ref="mapContainer" id="map" class="h-full w-full"></div>
 
-        <div class="absolute top-4 left-4 z-999">
+        <div class="absolute top-2 inset-x-0 flex justify-between z-999 px-4">
             <div
-                class="flex items-center gap-2 w-full px-3 py-2.5 rounded-lg bg-white dark:bg-neutral-900 border border-gray-300 dark:border-neutral-700 focus-within:border-green-500 focus-within:ring-2 focus-within:ring-green-500 transition"
+                class="flex items-center gap-2 w-full max-w-md px-3 py-2.5 rounded-lg bg-white dark:bg-neutral-900 border border-gray-300 dark:border-neutral-700 focus-within:border-green-500 focus-within:ring-2 focus-within:ring-green-500 transition"
             >
                 <!-- Icon -->
                 <svg
@@ -40,7 +40,7 @@ onBeforeUnmount(() => {
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    class="lucide lucide-search-icon lucide-search text-gray-400 dark:text-gray-500"
+                    class="text-gray-400 dark:text-gray-500"
                 >
                     <path d="m21 21-4.34-4.34" />
                     <circle cx="11" cy="11" r="8" />
@@ -52,6 +52,82 @@ onBeforeUnmount(() => {
                     placeholder="Full name"
                     class="flex-1 bg-transparent text-black dark:text-white placeholder-gray-400 dark:placeholder-gray-500 focus:outline-none"
                 />
+            </div>
+
+            <div class="flex gap-x-2">
+                <!--- ISSUE: Change this into offcanvas or modal button --->
+                <div
+                    class="flex items-center justify-center py-2 px-3 bg-white dark:bg-neutral-900 border border-gray-300 dark:border-neutral-700 rounded-lg shadow-md transition"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="lucide lucide-funnel-plus-icon lucide-funnel-plus text-green-500 dark:text-green-600"
+                    >
+                        <path
+                            d="M13.354 3H3a1 1 0 0 0-.742 1.67l7.225 7.989A2 2 0 0 1 10 14v6a1 1 0 0 0 .553.895l2 1A1 1 0 0 0 14 21v-7a2 2 0 0 1 .517-1.341l1.218-1.348"
+                        />
+                        <path d="M16 6h6" />
+                        <path d="M19 3v6" />
+                    </svg>
+                </div>
+
+                <!---  ISSUE: Change this into button that toggle between showing the map and the table VIEW component --->
+                <div
+                    class="flex items-center justify-center py-2 px-3 bg-white dark:bg-neutral-900 border border-gray-300 dark:border-neutral-700 rounded-lg shadow-md transition"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="lucide lucide-arrow-right-left-icon lucide-arrow-right-left text-green-500 dark:text-green-600"
+                    >
+                        <path d="m16 3 4 4-4 4" />
+                        <path d="M20 7H4" />
+                        <path d="m8 21-4-4 4-4" />
+                        <path d="M4 17h16" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+
+        <div class="absolute bottom-5 inset-x-0 flex justify-end z-999 px-4">
+            <div class="flex gap-x-2">
+                <!--- ISSUE: Change this a button that on and off polygon, and change the element to be button --->
+                <div
+                    class="flex items-center justify-center py-2 px-3 bg-white dark:bg-neutral-900 border border-gray-300 dark:border-neutral-700 rounded-lg shadow-md transition"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="lucide lucide-eye-icon lucide-eye text-green-500 dark:text-green-600"
+                    >
+                        <path
+                            d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"
+                        />
+                        <circle cx="12" cy="12" r="3" />
+                    </svg>
+                </div>
             </div>
         </div>
     </section>
