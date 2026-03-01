95c46b539aa5f19350d97059f8e71e177c002e4b
dev-nass <jonasvincemaca@gmail.com>
2026-03-01 16:02:04 +0800

    add: migrations

---
+++ database/migrations/2026_03_01_075233_create_burial_records_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\DeceasedRecord;
+use App\Models\Plot;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('burial_records', function (Blueprint $table) {
+            $table->id();
+
+            // added nullable to ensure nullOnDelete works
+            $table->foreignIdFor(Plot::class)->nullable()->constrained()->nullOnDelete();
+            $table->foreignIdFor(DeceasedRecord::class, 'deceased_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignIdFor(User::class)->nullable()->constrained()->nullOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('burial_records');
+    }
+};
---
+++ database/seeders/PanteonDataSeeder.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Section;
+use App\Models\Plot;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PanteonDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+
+    private function seedSections(): void
+    {
+        $geoJsonPath = public_path('data/sectiion.geojson');
+
+        if (!$geoJsonPath) {
+            $this->command->error("GeoJSON file for section not found at path $geoJsonPath");
+            return;
+        }
+
+        $this->command->info("Seeding sections from GeoJSON...");
+
+        foreach ($geoJsonPath['features'] as $feature) {
+            // $section_attributes = $feature['properties'];
+
+            $section_coords = [
+                'coordinates' => $feature['geometry'],
+            ];
+
+            Section::factory()->create($section_coords);
+        }
+
+        $this->command->info("Sections imported: " . count($geoJsonPath['features']));
+    }
+
+    private function seedPlots(): void
+    {
+        $geoJsonPath_Underground = public_path('data/lots_underground.geojson');
+        $geoJsonPath_Appartment = public_path('data/lots_appartment.geojson');
+
+        if (!file_exists($geoJsonPath_Underground) || !file_exists($geoJsonPath_Appartment)) {
+            $this->command->error("GeoJSON files not found");
+            return;
+        }
+
+        $geoJsonData_underground = json_decode(file_get_contents($geoJsonPath_Underground), true);
+        $geoJsonData_appartment = json_decode(file_get_contents($geoJsonPath_Appartment), true); // FIX: Was using Underground path
+
+        if (!isset($geoJsonData_underground['features']) || !isset($geoJsonData_appartment['features'])) {
+            $this->command->error("Invalid GeoJSON format: 'features' key not found.");
+            return;
+        }
+
+        $this->command->info("Seeding lots from GeoJSON...");
+
+        /**
+        * If I remember correctly, foreach create a shallow coppy of the array,
+        * meaning that if I states $features['properties']['lot_type'] = 'underground'
+        * It won't change the actual value just the shallow copy
+        * */
+        // Add lot_type to each feature's properties
+        // REFERENCE OPERATOR
+        foreach ($geoJsonData_underground['features'] as &$feature) {
+            $feature['properties']['lot_type'] = 'underground';
+            unset($feature); // Break reference
+        }
+
+        foreach ($geoJsonData_appartment['features'] as &$feature) {
+            $feature['properties']['lot_type'] = 'apartment';
+            unset($feature); // Break reference
+        }
+
+        $allFeatures = array_merge(
+            $geoJsonData_underground['features'],
+            $geoJsonData_appartment['features']
+        );
+
+        foreach ($allFeatures as $feature) {
+            $attributes  = $feature['properties'];
+
+            $lot = [
+                'section_id' => $attributes['section_id'],
+                'lot_type' => $attributes['lot_type'],
+                'coordinates' => $feature['geometry'],
+            ];
+
+            Plot::factory()->create($lot);
+        }
+
+        $this->command->info("Underground lots imported: " . count($geoJsonData_underground['features']));
+        $this->command->info("Appartment lots imported: " . count($geoJsonData_appartment['features']));
+        $this->command->info("Total lots imported: " . count($allFeatures));
+    }
+}
