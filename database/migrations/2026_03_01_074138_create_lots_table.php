2473d3b378e5a198957c7815d6c46e4b45306b3d
dev-nass <jonasvincemaca@gmail.com>
2026-03-01 16:48:33 +0800

    add: lot migrations

--- database/migrations/2026_03_01_074138_create_lots_table.php
+++ database/migrations/2026_03_01_074138_create_lots_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Section;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,6 +13,14 @@ return new class extends Migration {
     {
         Schema::create('lots', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignIdFor(Section::class)->constrained()->onDelete('cascade');
+            $table->string('lot_number')->unique();
+            $table->enum('lot_type', ['underground', 'apartment', 'columbarium']);
+            $table->enum('status', ['available', 'occupied', 'reserved'])->default('available');
+            $table->bigInteger('total_capacity')->nullable();
+            $table->json('coordinates');
+
             $table->timestamps();
         });
     }
