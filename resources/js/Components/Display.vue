f3e7ac58b8934312f88632f288da523ec879ba58
dev-nass <jonasvincemaca@gmail.com>
2026-03-07 18:06:22 +0800

    add: editing frontend logic

--- resources/js/Components/Display.vue
+++ resources/js/Components/Display.vue
@@ -13,12 +13,6 @@ const props = defineProps({
 });
 
 const emit = defineEmits(["update:modelValue"]);
-
-const displayValue = computed(() => props.modelValue ?? props.value);
-
-const updateValue = (event) => {
-    emit("update:modelValue", event.target.value);
-};
 </script>
 
 <template>
@@ -28,8 +22,8 @@ const updateValue = (event) => {
         </label>
         <input
             :type="type"
-            :value="displayValue"
-            @input="updateValue"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
             :disabled="!editing"
             placeholder="—"
             class="mt-1 w-full rounded-lg border border-gray-300 dark:border-neutral-700 bg-gray-100 dark:bg-neutral-800 text-gray-800 dark:text-white px-3 py-2 text-sm transition disabled:border-none disabled:bg-white disabled:dark:bg-neutral-900 disabled:dark:text-gray-200 disabled:hover:text-green-600 disabled:hover:dark:text-green-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
--- resources/js/Pages/Clerk/BurialRecords/ShowView.vue
+++ resources/js/Pages/Clerk/BurialRecords/ShowView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { usePage, router } from "@inertiajs/vue3";
 
 import Display from "@/Components/Display.vue";
 import Dashboard from "@/Layouts/Dashboard.vue";
+import { has } from "lodash";
 
 const props = defineProps({
     burial_record: { type: Object, required: true },
@@ -11,9 +12,7 @@ const props = defineProps({
 
 console.log(props.burial_record);
 
-const editing = ref(false);
 const activeTab = ref("personal");
-
 const tabs = [
     { key: "personal", label: "Personal Info" },
     { key: "death", label: "Death Info" },
@@ -23,6 +22,43 @@ const tabs = [
 
 const back = () => {
     window.history.back();
+};
+
+const editing = ref(false);
+const hasChanges = ref(false);
+
+// deep copy original data
+const originalData = JSON.parse(
+    JSON.stringify(props.burial_record.data.deceased),
+);
+const localData = ref(JSON.parse(JSON.stringify(originalData)));
+
+const modelValueChange = () => {
+    hasChanges.value = true;
+};
+
+watch(
+    localData,
+    (newVal) => {
+        hasChanges.value =
+            JSON.stringify(newVal) !== JSON.stringify(originalData);
+    },
+    { deep: true },
+);
+
+const discardChanges = () => {
+    if (hasChanges.value) {
+        if (confirm("Discard changes?")) {
+            localData.value = JSON.parse(JSON.stringify(originalData));
+            hasChanges.value = false;
+            editing.value = false;
+        }
+    }
+    editing.value = false;
+};
+
+const saveChanges = () => {
+    alert("saving...");
 };
 
 defineOptions({
@@ -101,15 +137,15 @@ defineOptions({
 
                 <div class="flex gap-x-3">
                     <button
-                        v-if="editing"
-                        @click="editing = !editing"
+                        v-if="hasChanges"
+                        @click="saveChanges"
                         class="flex items-center justify-center gap-x-2 mt-4 px-4 py-2 bg-green-500/10 text-green-400 rounded-xl border border-transparent hover:bg-green-500/20 hover:border-green-500/40 hover:text-green-600 dark:hover:text-green-300 transition-all duration-200"
                     >
                         Save Changes
                     </button>
                     <button
                         v-if="editing"
-                        @click="editing = !editing"
+                        @click="discardChanges"
                         class="flex items-center justify-center gap-x-2 mt-4 px-4 py-2 rounded-xl border border-transparent dark:bg-neutral-800 hover:dark:bg-neutral-600 transition-all duration-200"
                     >
                         Discard
@@ -145,48 +181,67 @@ defineOptions({
                 class="grid grid-cols-1 md:grid-cols-2 gap-6"
             >
                 <Display
+                    label="First Name"
+                    :modelValue="burial_record.data.deceased?.first_name"
+                    :editing="editing"
+                    @update:modelValue="modelValueChange"
+                />
+
+                <Display
+                    label="Middle Name"
+                    :modelValue="burial_record.data.deceased?.middle_name"
+                    :editing="editing"
+                    @update:modelValue="modelValueChange"
+                />
+                <Display
+                    label="Last Name"
+                    :modelValue="burial_record.data.deceased?.last_name"
+                    :editing="editing"
+                    @update:modelValue="modelValueChange"
+                />
+                <Display
                     label="Age"
-                    :value="burial_record.data.deceased?.age"
+                    :modelValue="burial_record.data.deceased?.age"
                     :editing="editing"
                 />
                 <Display
                     label="Date of Birth"
-                    :value="burial_record.data.deceased?.birth?.date"
+                    :modelValue="burial_record.data.deceased?.birth?.date"
                     :editing="editing"
                 />
                 <Display
                     label="Civil Status"
-                    :value="burial_record.data.deceased?.civil_status"
+                    :modelValue="burial_record.data.deceased?.civil_status"
                     :editing="editing"
                 />
                 <Display
                     label="Religion"
-                    :value="burial_record.data.deceased?.religion"
+                    :modelValue="burial_record.data.deceased?.religion"
                     :editing="editing"
                 />
                 <Display
                     label="Nationality"
-                    :value="burial_record.data.deceased?.nationality"
+                    :modelValue="burial_record.data.deceased?.nationality"
                     :editing="editing"
                 />
                 <Display
                     label="Occupation"
-                    :value="burial_record.data.deceased?.occupation?.name"
+                    :modelValue="burial_record.data.deceased?.occupation?.name"
                     :editing="editing"
                 />
                 <Display
                     label="Address"
-                    :value="burial_record.data.deceased?.address"
+                    :modelValue="burial_record.data.deceased?.address"
                     :editing="editing"
                 />
                 <Display
                     label="Part of LGBTQ"
-                    :value="burial_record.data.deceased?.lgbtq"
+                    :modelValue="burial_record.data.deceased?.lgbtq"
                     :editing="editing"
                 />
                 <Display
                     label="Precinct Number"
-                    :value="burial_record.data.deceased?.precinct_num"
+                    :modelValue="burial_record.data.deceased?.precinct_num"
                     :editing="editing"
                 />
             </div>
@@ -198,17 +253,17 @@ defineOptions({
             >
                 <Display
                     label="Date of Death"
-                    :value="burial_record.data.deceased?.death?.date"
+                    :modelValue="burial_record.data.deceased?.death?.date"
                     :editing="editing"
                 />
                 <Display
                     label="Cause of Death"
-                    :value="burial_record.data.deceased?.death?.cause"
+                    :modelValue="burial_record.data.deceased?.death?.cause"
                     :editing="editing"
                 />
                 <Display
                     label="Place of Death"
-                    :value="burial_record.data.deceased?.death?.place"
+                    :modelValue="burial_record.data.deceased?.death?.place"
                     :editing="editing"
                 />
             </div>
@@ -220,27 +275,27 @@ defineOptions({
             >
                 <Display
                     label="Corpse Disposal"
-                    :value="burial_record.data.deceased?.corpse_disposal"
+                    :modelValue="burial_record.data.deceased?.corpse_disposal"
                     :editing="editing"
                 />
                 <Display
                     label="Cremation Place"
-                    :value="burial_record.data.deceased?.cremation?.place"
+                    :modelValue="burial_record.data.deceased?.cremation?.place"
                     :editing="editing"
                 />
                 <Display
                     label="Cremation Date"
-                    :value="burial_record.data.deceased?.cremation?.date"
+                    :modelValue="burial_record.data.deceased?.cremation?.date"
                     :editing="editing"
                 />
                 <Display
                     label="Burial Place"
-                    :value="burial_record.data.deceased?.burial_place"
+                    :modelValue="burial_record.data.deceased?.burial_place"
                     :editing="editing"
                 />
                 <Display
                     label="Date of Depository"
-                    :value="burial_record.data.deceased?.burial?.date"
+                    :modelValue="burial_record.data.deceased?.burial?.date"
                     :editing="editing"
                 />
             </div>
@@ -252,22 +307,28 @@ defineOptions({
             >
                 <Display
                     label="Father's Name"
-                    :value="burial_record.data.deceased?.family?.father"
+                    :modelValue="burial_record.data.deceased?.family?.father"
                     :editing="editing"
                 />
                 <Display
                     label="Mother's Maiden Name"
-                    :value="burial_record.data.deceased?.family?.mother_maiden"
+                    :modelValue="
+                        burial_record.data.deceased?.family?.mother_maiden
+                    "
                     :editing="editing"
                 />
                 <Display
                     label="Company Address"
-                    :value="burial_record.data.deceased?.occupation?.address"
+                    :modelValue="
+                        burial_record.data.deceased?.occupation?.address
+                    "
                     :editing="editing"
                 />
                 <Display
                     label="Company Supervisor Name"
-                    :value="burial_record.data.deceased?.occupation?.supervisor"
+                    :modelValue="
+                        burial_record.data.deceased?.occupation?.supervisor
+                    "
                     :editing="editing"
                 />
             </div>
