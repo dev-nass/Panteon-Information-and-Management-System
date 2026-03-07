650388f5a14e8f3634b6b2adf090c3518d682d02
dev-nass <jonasvincemaca@gmail.com>
2026-03-07 13:29:04 +0800

    ref: display component hover effect

--- resources/js/Components/Display.vue
+++ resources/js/Components/Display.vue
@@ -10,7 +10,9 @@ defineProps({
         <p class="text-sm text-gray-500 dark:text-gray-400">
             {{ label }}
         </p>
-        <p class="text-base font-medium text-gray-800 dark:text-white">
+        <p
+            class="text-base font-medium text-gray-800 dark:text-white transition duration-300 hover:text-green-600 hover:dark:text-green-400"
+        >
             {{ value || "—" }}
         </p>
     </div>
