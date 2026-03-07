2c717caae8e0bea7370260afdfff6192cb04a8fe
dev-nass <jonasvincemaca@gmail.com>
2026-03-07 17:03:35 +0800

    add: editing ui on burial records

--- resources/js/Components/Display.vue
+++ resources/js/Components/Display.vue
@@ -1,19 +1,38 @@
 <script setup>
-defineProps({
+import { computed } from "vue";
+
+const props = defineProps({
     label: String,
+    modelValue: [String, Number],
     value: [String, Number],
+    editing: Boolean,
+    type: {
+        type: String,
+        default: "text",
+    },
 });
+
+const emit = defineEmits(["update:modelValue"]);
+
+const displayValue = computed(() => props.modelValue ?? props.value);
+
+const updateValue = (event) => {
+    emit("update:modelValue", event.target.value);
+};
 </script>
 
 <template>
     <div>
-        <p class="text-sm text-gray-500 dark:text-gray-400">
+        <label class="text-sm text-gray-500 dark:text-gray-400">
             {{ label }}
-        </p>
-        <p
-            class="text-base font-medium text-gray-800 dark:text-white transition duration-300 hover:text-green-600 hover:dark:text-green-400"
-        >
-            {{ value || "—" }}
-        </p>
+        </label>
+        <input
+            :type="type"
+            :value="displayValue"
+            @input="updateValue"
+            :disabled="!editing"
+            placeholder="—"
+            class="mt-1 w-full rounded-lg border border-gray-300 dark:border-neutral-700 bg-gray-100 dark:bg-neutral-800 text-gray-800 dark:text-white px-3 py-2 text-sm transition disabled:border-none disabled:bg-white disabled:dark:bg-neutral-900 disabled:dark:text-gray-200 disabled:hover:text-green-600 disabled:hover:dark:text-green-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
+        />
     </div>
 </template>
--- resources/js/Pages/Clerk/BurialRecords/ShowView.vue
+++ resources/js/Pages/Clerk/BurialRecords/ShowView.vue
@@ -11,6 +11,7 @@ const props = defineProps({
 
 console.log(props.burial_record);
 
+const editing = ref(false);
 const activeTab = ref("personal");
 
 const tabs = [
@@ -39,36 +40,82 @@ defineOptions({
         >
             ← Back
         </button>
-        <div class="mb-6 flex gap-x-3">
-            <div
-                class="flex items-center justify-center size-13 rounded-full bg-green-500/10 text-green-600 dark:text-green-400"
-            >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="22"
-                    height="22"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="lucide lucide-user-round-icon lucide-user-round"
+        <div class="mb-6 flex items-center justify-between">
+            <div class="flex gap-x-3">
+                <div
+                    class="flex items-center justify-center size-13 rounded-full bg-green-500/10 text-green-600 dark:text-green-400"
                 >
-                    <circle cx="12" cy="8" r="5" />
-                    <path d="M20 21a8 8 0 0 0-16 0" />
-                </svg>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="22"
+                        height="22"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="lucide lucide-user-round-icon lucide-user-round"
+                    >
+                        <circle cx="12" cy="8" r="5" />
+                        <path d="M20 21a8 8 0 0 0-16 0" />
+                    </svg>
+                </div>
+                <article>
+                    <h1
+                        class="text-2xl font-bold text-green-600 dark:text-green-400"
+                    >
+                        {{ burial_record.data.deceased.full_name }}
+                    </h1>
+                    <p class="text-sm text-gray-500 dark:text-gray-400">
+                        Deceased Record Details
+                    </p>
+                </article>
             </div>
-            <article>
-                <h1
-                    class="text-2xl font-bold text-green-600 dark:text-green-400"
+
+            <div>
+                <button
+                    v-if="!editing"
+                    @click="editing = !editing"
+                    class="flex items-center justify-center gap-x-2 mt-4 px-4 py-2 bg-green-500/10 text-green-400 rounded-xl border border-transparent hover:bg-green-500/20 hover:border-green-500/40 hover:text-green-600 dark:hover:text-green-300 transition-all duration-200"
                 >
-                    {{ burial_record.data.deceased.full_name }}
-                </h1>
-                <p class="text-sm text-gray-500 dark:text-gray-400">
-                    Deceased Record Details
-                </p>
-            </article>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="lucide lucide-pencil-icon lucide-pencil"
+                    >
+                        <path
+                            d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"
+                        />
+                        <path d="m15 5 4 4" />
+                    </svg>
+                    Edit
+                </button>
+
+                <div class="flex gap-x-3">
+                    <button
+                        v-if="editing"
+                        @click="editing = !editing"
+                        class="flex items-center justify-center gap-x-2 mt-4 px-4 py-2 bg-green-500/10 text-green-400 rounded-xl border border-transparent hover:bg-green-500/20 hover:border-green-500/40 hover:text-green-600 dark:hover:text-green-300 transition-all duration-200"
+                    >
+                        Save Changes
+                    </button>
+                    <button
+                        v-if="editing"
+                        @click="editing = !editing"
+                        class="flex items-center justify-center gap-x-2 mt-4 px-4 py-2 rounded-xl border border-transparent dark:bg-neutral-800 hover:dark:bg-neutral-600 transition-all duration-200"
+                    >
+                        Discard
+                    </button>
+                </div>
+            </div>
         </div>
 
         <!-- Tabs -->
@@ -97,38 +144,50 @@ defineOptions({
                 v-if="activeTab === 'personal'"
                 class="grid grid-cols-1 md:grid-cols-2 gap-6"
             >
-                <Display label="Age" :value="burial_record.data.deceased.age" />
+                <Display
+                    label="Age"
+                    :value="burial_record.data.deceased?.age"
+                    :editing="editing"
+                />
                 <Display
                     label="Date of Birth"
-                    :value="burial_record.data.deceased.birth.date"
+                    :value="burial_record.data.deceased?.birth?.date"
+                    :editing="editing"
                 />
                 <Display
                     label="Civil Status"
-                    :value="burial_record.data.deceased.civil_status"
+                    :value="burial_record.data.deceased?.civil_status"
+                    :editing="editing"
                 />
                 <Display
                     label="Religion"
-                    :value="burial_record.data.deceased.religion"
+                    :value="burial_record.data.deceased?.religion"
+                    :editing="editing"
                 />
                 <Display
                     label="Nationality"
-                    :value="burial_record.data.deceased.nationality"
+                    :value="burial_record.data.deceased?.nationality"
+                    :editing="editing"
                 />
                 <Display
                     label="Occupation"
-                    :value="burial_record.data.deceased.occupation.name"
+                    :value="burial_record.data.deceased?.occupation?.name"
+                    :editing="editing"
                 />
                 <Display
                     label="Address"
-                    :value="burial_record.data.deceased.address"
+                    :value="burial_record.data.deceased?.address"
+                    :editing="editing"
                 />
                 <Display
                     label="Part of LGBTQ"
-                    :value="burial_record.data.deceased.lgbtq"
+                    :value="burial_record.data.deceased?.lgbtq"
+                    :editing="editing"
                 />
                 <Display
                     label="Precinct Number"
-                    :value="burial_record.data.deceased.precinct_num"
+                    :value="burial_record.data.deceased?.precinct_num"
+                    :editing="editing"
                 />
             </div>
 
@@ -139,15 +198,18 @@ defineOptions({
             >
                 <Display
                     label="Date of Death"
-                    :value="burial_record.data.deceased.death.date"
+                    :value="burial_record.data.deceased?.death?.date"
+                    :editing="editing"
                 />
                 <Display
                     label="Cause of Death"
-                    :value="burial_record.data.deceased.death.cause"
+                    :value="burial_record.data.deceased?.death?.cause"
+                    :editing="editing"
                 />
                 <Display
                     label="Place of Death"
-                    :value="burial_record.data.deceased.death.place"
+                    :value="burial_record.data.deceased?.death?.place"
+                    :editing="editing"
                 />
             </div>
 
@@ -158,23 +220,28 @@ defineOptions({
             >
                 <Display
                     label="Corpse Disposal"
-                    :value="burial_record.data.deceased.corpse_disposal"
+                    :value="burial_record.data.deceased?.corpse_disposal"
+                    :editing="editing"
                 />
                 <Display
                     label="Cremation Place"
                     :value="burial_record.data.deceased?.cremation?.place"
+                    :editing="editing"
                 />
                 <Display
                     label="Cremation Date"
                     :value="burial_record.data.deceased?.cremation?.date"
+                    :editing="editing"
                 />
                 <Display
                     label="Burial Place"
-                    :value="burial_record.data.deceased.burial_place"
+                    :value="burial_record.data.deceased?.burial_place"
+                    :editing="editing"
                 />
                 <Display
                     label="Date of Depository"
-                    :value="burial_record.data.deceased.burial.date"
+                    :value="burial_record.data.deceased?.burial?.date"
+                    :editing="editing"
                 />
             </div>
 
@@ -185,19 +252,23 @@ defineOptions({
             >
                 <Display
                     label="Father's Name"
-                    :value="burial_record.data.deceased.family.father"
+                    :value="burial_record.data.deceased?.family?.father"
+                    :editing="editing"
                 />
                 <Display
                     label="Mother's Maiden Name"
-                    :value="burial_record.data.deceased.family.mother_maiden"
+                    :value="burial_record.data.deceased?.family?.mother_maiden"
+                    :editing="editing"
                 />
                 <Display
                     label="Company Address"
-                    :value="burial_record.data.deceased.occupation.address"
+                    :value="burial_record.data.deceased?.occupation?.address"
+                    :editing="editing"
                 />
                 <Display
                     label="Company Supervisor Name"
-                    :value="burial_record.data.deceased.occupation.supervisor"
+                    :value="burial_record.data.deceased?.occupation?.supervisor"
+                    :editing="editing"
                 />
             </div>
         </div>
