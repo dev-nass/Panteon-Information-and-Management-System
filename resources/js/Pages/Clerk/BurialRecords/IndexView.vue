6b45184fb39cb4d28d03394c4014fb543fb5f43b
dev-nass <jonasvincemaca@gmail.com>
2026-04-07 21:13:48 +0800

    ref: removed animation as Sir Ketch suggestion

--- resources/js/Pages/Clerk/BurialRecords/IndexView.vue
+++ resources/js/Pages/Clerk/BurialRecords/IndexView.vue
@@ -68,7 +68,7 @@ defineOptions({
     <!-- Table Section -->
     <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-6 mx-auto">
         <!-- Card -->
-        <div class="flex flex-col" data-aos="zoom-out">
+        <div class="flex flex-col">
             <div class="-m-1.5 overflow-x-auto">
                 <div class="p-1.5 min-w-full inline-block align-middle">
                     <div
