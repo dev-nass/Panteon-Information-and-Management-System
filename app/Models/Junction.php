7217c0aa7b86b837cebedf4033cf9662decf7b8e
dev-nass <jonasvincemaca@gmail.com>
2026-04-09 21:26:18 +0800

    add: all neccessary files

---
+++ app/Models/Junction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Junction extends Model
+{
+    /** @use HasFactory<\Database\Factories\JunctionFactory> */
+    use HasFactory;
+}
