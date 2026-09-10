f7fbd7fd5f44384cea04f04e36d396536ed73e4b
dev-nass <jonasvincemaca@gmail.com>
2026-09-10 12:53:09 +0800

    add: favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" href="/images/dasmarinas-logo.png" />
+    <link rel="apple-touch-icon" href="/images/dasmarinas-logo.png" />
     @routes <!-- Ziggy routes -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
