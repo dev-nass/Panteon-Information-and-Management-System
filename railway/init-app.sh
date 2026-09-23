2e226079ff8a61dccfb36a5bbcb6dbc6ca371de7
dev-nass <jonasvincemaca@gmail.com>
2026-09-23 16:56:00 +0800

    Add Railway deploy scripts

---
+++ railway/init-app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+php artisan migrate --force --seed
+php artisan optimize:clear
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
