5cbd601cbbbea573620dbac0016885569d65e189
dev-nass <jonasvincemaca@gmail.com>
2026-09-24 14:15:52 +0800

    ref: middleware trust railway proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,9 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -24,15 +24,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'clerk' => ClerkMiddleware::class,
             'admin' => AdminMiddleware::class,
         ]);
-        // TUNNELING CONFIG
-        $middleware->trustProxies(
-            at: '**',
-            headers: Request::HEADER_X_FORWARDED_FOR |
-            Request::HEADER_X_FORWARDED_HOST |
-            Request::HEADER_X_FORWARDED_PORT |
-            Request::HEADER_X_FORWARDED_PROTO |
-            Request::HEADER_X_FORWARDED_AWS_ELB
-        );
+        // Trust Railway's load balancer / reverse proxy
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
